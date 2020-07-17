f6b62298603d1b8b253e9fee5314e6674d27946f
viktor-lima <joaolimak10@outlook.com>
2020-07-17 03:06:29 -0300

    bloco try-with-resources

--- src/files/aula2/Program.java
+++ src/files/aula2/Program.java
@@ -7,13 +7,9 @@ import java.io.IOException;
 public class Program {
 	public static void main(String[] args) {
 		String path = "c:\\temp\\in.txt";
-		FileReader fr = null;
-		BufferedReader br =null;
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 			while (line != null) {
 				System.out.println(line);
@@ -22,19 +18,6 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} 
-			catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
